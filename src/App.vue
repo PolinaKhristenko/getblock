048dc3f8c80b8803d0925a7b2419b7ae11735abb
PolinaTimofeeva <polleyw@mail.ru>
2022-06-08 22:23:10 +0300

    setup completed

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+      <HelloWorld></HelloWorld>
+  </div>
 </template>
 
 <script>
@@ -14,13 +15,4 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style src="./assets/styles/tailwind.css"></style>
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: {
+    content: ['./public/**/*.html', './src/**/*.vue'],
+    options: {
+      whitelistPatterns: [
+        /-(leave|enter|appear)(|-(to|from|active))$/,
+        /^(?!(|.*?:)cursor-move).+-move$/,
+        /^router-link(|-exact)-active$/,
+        /tooltip/,
+        /popover/,
+        /notification/,
+      ],
+    }
+  },
+  // ...
+}
