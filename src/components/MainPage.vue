2edf177bbf5ba5e9d970b1730729e2747923fa11
PolinaTimofeeva <polleyw@mail.ru>
2022-06-10 18:09:48 +0300

    main functionality

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -2,30 +2,33 @@
   <main>
     <section>
       <div class="max-w-[1000px] mx-auto ">
-        <h1 class="text-h1 text-black font-light mb-4">Crypto Exchange</h1>
+        <h1 class="md:text-h1 text-[40px] text-black font-light mb-4">Crypto Exchange</h1>
         <h6 class="text-xl text-black">Exchange fast and easy</h6>
 
-        <p v-if="error" class="text-[#c12d2d]">{{error}}</p> <!--Error-->
+        <div class="flex items-center mt-16"> 
 
-        <div class="flex items-center mt-16 mb-8"> 
-
-          <div class="dropdown__wrapper relative" v-bind:class="{ active: fromIsActive }"> <!--FROM currency-->
+          <div class="dropdown__wrapper max-w-md w-full relative" v-bind:class="{ active: fromIsActive }"> <!--FROM currency-->
 
               <input type="text" @input="changeFrom($event)"
               :placeholder="'Search'" :value="`${minimalVal}`"
               class="border border-[#E3EBEF] border-solid py-3.5 pl-4 
-              rounded-[5px] pr-3"/> <!--Value input-->
+              rounded-[5px] pr-3 max-w-md w-full"/> <!--Value input-->
 
-              <div class="absolute top-2 right-2 w-10 h-10 flex " @click="toggleFrom()">
-                <img v-if="chosenMin.img" :src="chosenMin.img" alt='Crypto image' class="w-5 h-5"/>
-                <p v-if="chosenMin.ticker" class="uppercase">{{chosenMin.ticker}}</p>
+              <div class="absolute top-2 h-10 flex cursor-pointer
+              border-l border-gray border-solid items-center pl-9 right-6" 
+              @click="toggleFrom()">
+                <img v-if="chosenMin.img" :src="chosenMin.img" alt='Crypto image' class="w-5 h-5 mr-3.5"/>
+                <p v-if="chosenMin.ticker" class="uppercase mr-7 text-black">{{chosenMin.ticker}}</p>
                 <img src="../assets/img/arrow-down.svg" class="w-4 h-4" alt="Options arrow"/>
               </div> <!--Chosen currency-->
 
               <div class="dropdown__options"> <!--Dropdown-->
-                <ul class="dropdown__list absolute">
+                <ul class="dropdown__list w-full absolute top-[-54px] left-0 border 
+                border-[#C1D9E5] border-solid h-36 overflow-y-scroll rounded-[5px]">
+
                   <input type="search" v-model="fromSearch" 
-                  class="py-3.5 pl-4 pr-3 rounded-[5px]"
+                  class="py-3.5 pl-4 pr-3 rounded-[5px] max-w-md w-full 
+                  border-b border-[#E3EBEF] border-solid"
                   :placeholder="'Search'" /> <!--Search input-->
 
                   <li @click="setFrom(crypto.ticker, crypto.image)"
@@ -33,12 +36,14 @@
                     class="items-center hover:bg-light-blue bg-white"> 
                     <a class="flex py-3 pl-5">
                       <img v-if="crypto.image" :src="crypto.image" alt='Crypto image' class="w-5 h-5"/>
-                      <p class="mx-3 uppercase">{{crypto.ticker}}</p><p>{{crypto.name}}</p>
+                      <p class="mx-3 uppercase text-black">{{crypto.ticker}}</p>
+                      <p class="text-[#80A2B6]">{{crypto.name}}</p>
                     </a>
                   </li> <!--Crypto info-->
                 </ul>
               </div>
           </div>
+
 
           <a class="mx-7 flex"> <!--Exchange sign-->
             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -47,30 +52,39 @@
             </svg>
           </a>
           
-          <div class="dropdown__wrapper relative" v-bind:class="{ active: toIsActive }">  <!--TO currency-->
+
+          <div class="dropdown__wrapper max-w-md w-full relative" v-bind:class="{ active: toIsActive }">  <!--TO currency-->
 
               <input type="text" :placeholder="'Search'" 
               :value="`${estimatedVal}`"
               class="border border-[#E3EBEF] border-solid py-3.5 pl-4 
-              rounded-[5px] pr-3"/> <!--Value input-->
+              rounded-[5px] pr-3 max-w-md w-full"/> <!--Value input-->
 
-              <div class="absolute top-2 right-2 w-10 h-10 flex " @click="toggleTo()">
-                <img v-if="chosenEstim.img" :src="chosenEstim.img" alt='Crypto image' class="w-5 h-5"/>
-                <p v-if="chosenEstim.ticker" class="uppercase">{{chosenEstim.ticker}}</p>
+              <div class="absolute top-2 h-10 flex cursor-pointer
+              border-l border-gray border-solid items-center pl-9 right-6" 
+              @click="toggleTo()">
+                <img v-if="chosenEstim.img" :src="chosenEstim.img" alt='Crypto image' class="w-5 h-5 mr-3.5"/>
+                <p v-if="chosenEstim.ticker" class="uppercase mr-7 text-black">{{chosenEstim.ticker}}</p>
                 <img src="../assets/img/arrow-down.svg" class="w-4 h-4" alt="Options arrow"/>
               </div> <!--Chosen currency-->
 
               <div class="dropdown__options"> <!--Dropdown-->
-                <ul class="dropdown__list absolute">
+                <ul class="dropdown__list w-full absolute top-[-54px] left-0 border border-[#C1D9E5] 
+                border-solid h-36 overflow-y-scroll rounded-[5px]">
 
                   <input type="search" v-model="toSearch" 
-                  class="py-3.5 pl-4 pr-3 rounded-[5px]"
+                  class="py-3.5 pl-4 pr-3 rounded-[5px] max-w-md w-full
+                  border-b border-[#E3EBEF] border-solid"
                   :placeholder="'Search'" /> <!--Search input-->
 
                   <li @click="setTo(crypto.ticker, crypto.image)" 
                     v-for="crypto in toSearchHandler" v-bind:key="crypto"
                     class="items-center hover:bg-light-blue bg-white">
-                    <a class="flex py-3 pl-5"><img :src="`${crypto.image}`" alt='Crypto image' class="w-5 h-5"/><p class="mx-3 uppercase">{{crypto.ticker}}</p><p>{{crypto.name}}</p></a>
+                    <a class="flex py-3 pl-5">
+                      <img v-if="crypto.image" :src="crypto.image" alt='Crypto image' class="w-5 h-5"/>
+                      <p class="mx-3 uppercase text-black">{{crypto.ticker}}</p>
+                      <p class="text-[#80A2B6]">{{crypto.name}}</p>
+                    </a>
                   </li> <!--Crypto info-->
                 </ul>
               </div>
@@ -78,15 +92,17 @@
         </div>
 
         <div class="">
+          <p class="text-[#c12d2d] mb-8 h-3">{{error}}</p> <!--Minimal amoint error-->
           <p class="text-base text-black mb-2">Your Ethereum address</p>
           <div class="flex"> 
-            <input type="text" class="border border-[#E3EBEF] border-solid flex grow" />
-            <button class="flex text-white font-bold bg-blue submit__btn uppercase hover:bg-[#0095E0] ml-8" type="submit">Exchange</button>
+            <input type="text" class="border rounded-[5px] border-[#E3EBEF] border-solid flex grow" />
+            <button class="flex text-white font-bold bg-blue py-4 px-[59px]
+            uppercase hover:bg-[#0095E0] ml-8 rounded-[5px]" type="submit">Exchange</button>
           </div>
+          <p class="text-[#c12d2d] h-3">{{errorPair}}</p> <!--Disabled pair error -->
         </div>
       </div>
     </section>
-    <section></section>
   </main>
 </template>
 
@@ -99,6 +115,7 @@ export default {
       apiKey: 'c9155859d90d239f909d2906233816b26cd8cf5ede44702d422667672b58b0cd',
       cryptos: [],
       error: '',
+      errorPair: '',
       fromSearch: '',
       toSearch: '',
       chosenMin: {
@@ -133,6 +150,7 @@ export default {
       this.chosenMin.ticker = fromVal;
       this.chosenMin.img = fromImg;
       this.toggleFrom();
+      this.getMinimal();
     }, // Sets fromVal
     setTo (toVal, toImg) {
       this.toCrypto = toVal;
@@ -149,14 +167,18 @@ export default {
           const urlMin = `https://api.changenow.io/v1/min-amount/${from}_${to}?api_key=${this.apiKey}`;
           const responseMin = await axios.get(urlMin);
 
-          this.minimalVal = responseMin.data.minAmount;
+          if (responseMin) {
+            this.minimalVal = responseMin.data.minAmount;
+            this.getEstimated();
+          } else {
+            this.errorPair = 'This pair is disabled now'
+          }
 
-          this.getEstimated();
         } catch (err) {
           console.log(err);
         }
       } 
-    },  // Sets minimum amount and calls getEstimated function
+    }, // Sets minimum amount and calls getEstimated function
     async getEstimated() {
       if (this.fromCrypto && this.toCrypto && (this.minimalVal || this.newFromVal)) {
           const from = this.fromCrypto;
@@ -166,7 +188,12 @@ export default {
 
           const responseEstim = await axios.get(urlEstim);
 
-          this.estimatedVal = responseEstim.data.estimatedAmount;
+          if (responseEstim) {
+            this.estimatedVal = responseEstim.data.estimatedAmount;
+          } else {
+            this.errorPair = 'This pair is disabled now'
+          }
+
         } catch (err) {
           console.log(err);
         }
@@ -177,20 +204,19 @@ export default {
       if (this.newFromVal < this.minimalVal) {
         this.error = `This amount should be more than ${this.minimalVal}`;
         this.estimatedVal = `-`;
-        console.log(this.estimatedVal)
       } else {
         this.error = '';
         e.target.value = this.newFromVal;
         this.getEstimated();
       }
-    }, // Changes fromVal depending on input values
+    }, // Changes fromVal depending on input value
   },
   computed: {
     fromSearchHandler() {
       return this.cryptos.filter(element => {
         return element.name.toLowerCase().includes(this.fromSearch.toLowerCase());
       })
-    }, 
+    }, // Search 
     toSearchHandler() {
       return this.cryptos.filter(element => {
         return element.name.toLowerCase().includes(this.toSearch.toLowerCase());
@@ -205,37 +231,14 @@ export default {
 </script>
 
 <style scoped>
-  .dropdown__wrapper, 
-  .dropdown__wrapper input{
-    max-width: 440px;
-    width: 100%;
-  }
-
   .dropdown__options {
     display: none;
     transform: scaleY(0);
     position: relative;
   }
 
-  .dropdown__wrapper.active .dropdown__options{
+  .dropdown__wrapper.active .dropdown__options {
     transform: scaleY(1);
     display: flex;
   }
-
-  .dropdown__list {
-    width: 100%;
-    top: -54px;
-    left: 0;
-    border: 1px solid #C1D9E6;
-    height: 144px;
-    overflow-y: scroll;
-  }
-
-
-  .submit__btn {
-    padding: 15px 59px;
-    border-radius: 5px;
-  }
-
-  
 </style>
